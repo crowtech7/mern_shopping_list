1951dca88205045a1bd5e8e4baf9b96d37195028
Talin Crow <crowtech7@gmail.com>
2020-10-02 13:58:10 -0500

    First commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://dbuser:Password1!@cluster0-2ofn3.mongodb.net/mern?retryWrites=true&w=majority'
+};
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+//const bodyParser = require('body-parser'); in video
+
+const items = require('./routes/api/items');
+
+const app = express();
+    
+    //Bodypareser Middleware
+  //  app.use(bodyPareser.json()); in video
+  app.use(express.json());
+
+  // DB config
+  const db = require('./config/keys').mongoURI;
+
+  //Connect to Mongo
+  mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+  //Use Routes
+  app.use('/api/items', items)
+
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => console.log(`Server started on port ${port}`))

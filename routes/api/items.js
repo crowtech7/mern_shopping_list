8aa222989b9cdc51ed4c7286fa318b2859a58624
Talin Crow <crowtech7@gmail.com>
2020-10-02 16:12:45 -0500

    Added Shopping List component and static data in state.

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,12 +14,24 @@ router.get('/', (req, res) => {
 });
 
 // @route   POST api/items
-// @desc    Create a Post
+// @desc    Create a Item
 // @access  Public
 router.post('/', (req, res) => {
     const newItem = new Item({
         name: req.body.name
     });
+
+    newItem.save().then(item => res.json(item));
 });
+
+// @route   DELETE api/items/:id
+// @desc    Delete a Item
+// @access  Public
+router.delete('/:id', (req, res) => {
+Item.findById(req.params.id)
+.then(item => item.remove().then(() => res.json({success: true})))
+.catch(err => res.status(404).json({ success: false}))
+});
+
 
 module.exports = router;

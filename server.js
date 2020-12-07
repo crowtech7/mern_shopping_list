c90487b28a80d607a575af44a6d25d48bcc3a46f
Talin Crow <crowtech7@gmail.com>
2020-12-07 16:07:46 -0600

    Added Backend JWT Auth

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
-//const bodyParser = require('body-parser'); in video
 const path = require('path');
-
-const items = require('./routes/api/items');
+const config = require('config');
 
 const app = express();
-    
-    //Bodypareser Middleware
-  //  app.use(bodyPareser.json()); in video
-  app.use(express.json());
 
-  // DB config
-  const db = require('./config/keys').mongoURI;
+//Bodypareser Middleware
+app.use(express.json());
 
-  //Connect to Mongo
-  mongoose
+// DB config
+const db = config.get("mongoURI");
+
+//Connect to Mongo
+mongoose
   .connect(db, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-    })
+    useCreateIndex: true
+  })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-  //Use Routes
-  app.use('/api/items', items);
+//Use Routes
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
-  // Serve static assests if in production
-  if (process.env.NODE_ENV === 'production') {
-    //Set static folder
-    app.use(express.static('client/build'));
+// Serve static assests if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
-  const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-  app.listen(port, () => console.log(`Server started on port ${port}`))
+app.listen(port, () => console.log(`Server started on port ${port}`))
